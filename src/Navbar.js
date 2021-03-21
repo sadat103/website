e406bf9af1aa7c8caf3bb6584656d05acc124594
sadat103 <1505103.sbf@ugrad.cse.buet.ac.bd>
2021-03-21 18:30:59 +0600

    done

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () =>
 {
@@ -12,33 +13,30 @@ const Navbar = () =>
                   
             
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
-               <a className="navbar-brand" href="#">Navbar</a>
+               <NavLink className="navbar-brand" to="#">Sadat's Website</NavLink>
                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                  <span className="navbar-toggler-icon"></span>
                </button>
 
                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav ml-auto">
+                <ul className="navbar-nav m-auto">
                  <li className="nav-item active">
-                   <a className="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                   <NavLink activeClassName='menu_active'  className="nav-link" to="/">Home <span class="sr-only">(current)</span></NavLink>
                 </li>
                  <li className="nav-item">
-                   <a className="nav-link" href="#">About</a>
+                   <NavLink activeClassName='menu_active' className="nav-link" to="/about">About</NavLink>
                  </li>
 
       
                  <li className="nav-item">
-                   <a className="nav-link" href="#">Service</a>
+                   <NavLink activeClassName='menu_active' className="nav-link" to="/service">Service</NavLink>
                  </li>
 
                 
                  <li className="nav-item">
-                   <a className="nav-link" href="#">Contact</a>
+                   <NavLink activeClassName='menu_active' className="nav-link" to="/contact">Contact</NavLink>
                  </li>
                
-                 <li className="nav-item">
-                   <a className="nav-link" href="#">Link</a>
-                 </li>
                 </ul>
                </div>
              </nav>
