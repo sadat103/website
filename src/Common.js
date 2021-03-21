31df0ed789ec061cdbc947e189d9d005e5e85dc6
sadat103 <1505103.sbf@ugrad.cse.buet.ac.bd>
2021-03-21 18:30:45 +0600

    page create done

---
+++ src/Common.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom';
+
+const Common = (props) =>
+{
+   return(
+     <>
+        
+        <section id="header" className="d-flex alignItems: 'center', ">
+                 
+          <div className="container-fluid nav_bg">
+             <div className='row'>
+
+
+                <div className="col-10 mx-auto">
+                   <div className="row">
+                   <div className="col-md-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex justify-content-center flex-column ">
+                      
+                      <h1>
+                          {props.name} <strong className="brand_name">Sadats website</strong>
+                      </h1>
+                      <h2 className="my-3"> we are team of talented</h2>
+                      <div className="mt-3"> 
+                          <NavLink to={props.visit} className="btn-get-started"> 
+                        {props.btname}
+                           </NavLink> 
+                      </div>
+                   </div>
+                  <div className="col-lg-6 order-1 order-lg-2 hrader-img">
+                     <img src={props.imgsrc} className="img-fluid animated" alt="home img" >
+                       
+                     </img>
+
+
+                  </div>
+                  </div>
+                </div>               
+             </div>
+
+
+         </div>
+          
+
+        </section>
+     </>
+
+
+   );
+
+
+};
+
+export default Common;
